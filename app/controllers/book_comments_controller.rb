62fbfaffe85944b2ea661c813403a10d1afbc312
yuken02 <git@github.com:yuken02/practice.git>
2022-11-23 13:17:30 +0000

    [Add]応用課題3　コメント機能

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,22 @@
+class BookCommentsController < ApplicationController
+
+  def create
+    book = Book.find(params[:book_id])
+    comment = current_user.book_comments.new(book_comment_params)
+    comment.book_id = book.id
+    comment.save
+    redirect_to book_path(book)
+  end
+
+  def destroy
+    book_comment = current_user.book_comments
+    BookComment.find(params[:id]).destroy
+    redirect_to book_path(book_comment.book)
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment, :user_id, :book_id)
+  end
+end

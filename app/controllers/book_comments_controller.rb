ed91fbd809f751f6c5bab6747929403a9889d558
yuken02 <git@github.com:yuken02/practice.git>
2022-12-05 07:59:19 +0000

    [Finish]課題6　コメントの非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,16 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
   end
 
   def destroy
-    book_comment = current_user.book_comments
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments
     BookComment.find(params[:id]).destroy
-    redirect_to request.referer
   end
 
   private

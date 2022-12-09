d0528c4479d0a456bc2a5c668f58692cf7892161
yuken02 <git@github.com:yuken02/practice.git>
2022-12-09 02:56:29 +0000

    [Fix]課題9b 3日〜6日前のデータ取得日数の修正

--- app/models/book.rb
+++ app/models/book.rb
@@ -29,9 +29,10 @@ class Book < ApplicationRecord
   scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
   scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
   scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
+
   scope :created_2days_ago, -> { where(created_at: 2.day.ago.all_day) }
-  scope :created_3days_ago, -> { where(created_at: 2.day.ago.all_day) }
-  scope :created_4days_ago, -> { where(created_at: 2.day.ago.all_day) }
-  scope :created_5days_ago, -> { where(created_at: 2.day.ago.all_day) }
-  scope :created_6days_ago, -> { where(created_at: 2.day.ago.all_day) }
+  scope :created_3days_ago, -> { where(created_at: 3.day.ago.all_day) }
+  scope :created_4days_ago, -> { where(created_at: 4.day.ago.all_day) }
+  scope :created_5days_ago, -> { where(created_at: 5.day.ago.all_day) }
+  scope :created_6days_ago, -> { where(created_at: 6.day.ago.all_day) }
 end

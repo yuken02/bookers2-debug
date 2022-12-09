489e702f163474394db27afbbd0f695455c7d47b
yuken02 <git@github.com:yuken02/practice.git>
2022-12-09 07:58:53 +0000

    [Start]課題7a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,14 +7,16 @@ class BooksController < ApplicationController
 
   def index
     @book = Book.new
-    # @books_chart = Book.all
-    # @books = Book.includes(:favorited_users).sort.reverse
     to  = Time.current.at_end_of_day
     from  = (to - 6.day).at_beginning_of_day
-    @books = Book.all.sort {|a,b|
+    @books = Book.includes(:favorites).sort {|a,b|
       b.favorites.where(created_at: from...to).size <=>
       a.favorites.where(created_at: from...to).size
+      # x.favorites.where(created_at: from...to).size
     }
+    @books_chart = Book.all
+
+    # @books = Book.includes(:favorited_users).sort.reverse
     # @books = Book.includes(:favorited_users).
     #   sort_by {|x|
     #     x.favorited_users.includes(:favorites).where(created_at: from...to).size
